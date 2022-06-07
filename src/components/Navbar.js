e832c42b2389f4408b511dd3e0d3373b1c84b5f1
gregoryong16 <gregory.ong16@gmail.com>
2022-06-07 23:50:59 +0800

    daily commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import { Avatar } from "@chakra-ui/react"
+import React from "react"
+import pic from "../images/icon.png"
+
+export default function Navbar() {
+    return <nav className="nav">
+        <button className="nav-profile_button">
+            <img src={pic} className="nav-icon" alt= "icon"/>
+            <h1 className="nav-profile_text">Gregory Ong</h1>
+        </button>
+        <button className="nav-education_button"> Education</button>
+        <button className="nav-skills_button"> Skills</button>
+        <button className="nav-work_button"> Work</button>
+        <button className="nav-projects_button"> Projects</button>
+        <button className="nav-cca_button"> CCA</button>
+        <button className="nav-achievements_button"> Achievements</button>
+        </nav> 
+}
