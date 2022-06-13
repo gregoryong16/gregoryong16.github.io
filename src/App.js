eda675a573e4729b69a25e7c8aafe04dcf4b5138
gregoryong16 <gregory.ong16@gmail.com>
2022-06-13 22:07:51 +0800

    changed website icon and name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './index.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import {Helmet} from "react-helmet";
 
 export default function App() {
   return (
     <div className="App">
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>Gregory's Portfolio Website</title>
+                {/* can change href to url u want accordingly */}
+                <link rel="canonical" href="http://mysite.com/example" /> 
+                <meta name="description" content="Gregory's Portfolio Website" />
+          </Helmet>
       <Navbar />
       <Main />
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Container, Box } from "@chakra-ui/react"
 import TypeAnimation from 'react-type-animation';
+import avatar from "../images/avatar.glb"
 
 export default function Main() {
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
     return <Container class = "main" >
         <div class="main-avatar">3D Avatar</div> 
         <Box className="main-intro-box">
