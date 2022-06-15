77cf0a886591f434523bf1e9c71901b2b53779e4
gregoryong16 <gregory.ong16@gmail.com>
2022-06-15 21:06:46 +0800

    added 3D avatar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,30 @@
-import React from "react"
 import { Container, Box } from "@chakra-ui/react"
 import TypeAnimation from 'react-type-animation';
-import avatar from "../images/avatar.glb"
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Avatar from "./Avatar"
 
 export default function Main() {
     <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
     return <Container class = "main" >
-        <div class="main-avatar">3D Avatar</div> 
+        <div class="main-avatar">3D Avatar</div>
+        <Canvas
+         camera={{ position: [2, 0, 12.25], fov: 15 }}
+         style={{
+            backgroundColor: '#111a21',
+            width: '100vw',
+            height: '100vh',
+         }}
+      >
+         <ambientLight intensity={1.25} />
+         <ambientLight intensity={0.1} />
+         <directionalLight intensity={0.4} />
+         <Suspense fallback={null}>
+            <Avatar position={[0.025, -0.9, 0]} /> 
+         </Suspense>
+         <OrbitControls />
+      </Canvas> 
         <Box className="main-intro-box">
             <h1 class="intro-name">Hi, I&apos;m Gregory!</h1>
             <div className = "intro-description" style={{ height: '40px' }} > 
