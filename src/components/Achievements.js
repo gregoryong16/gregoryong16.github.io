d96c00b7a057395612625f67c8945183ee7e381d
gregoryong16 <gregory.ong16@gmail.com>
2022-10-08 11:32:44 +0800

    changed nav bar responsiveness

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,47 +1,87 @@
-import React from "react"
-import {GrAchievement} from "react-icons/gr"
-import goldmansachs from "../images/goldmansachs.png"
+import React from "react";
+import { GrAchievement } from "react-icons/gr";
+import goldmansachs from "../images/goldmansachs.png";
 import AnimatedPage from "./AnimatedPage";
 
-const ACHIEVEMENTS= [
-    {
-        title: 'GoldMan Sachs Engage 2021 Finalist',
-		desc: (
-			<ul>
-                <li>Extracted Unstructured ESG data and performed topic modelling on reports</li>
-                <li>Topic modelling models used include Latent Dirichlet Allocation (LDA) </li>
-                <li>Performed multiclass Classification using Light Gradient Boosting Model</li>
-            </ul>
-		),
-		image: goldmansachs,
-    },
-
+const ACHIEVEMENTS = [
+  {
+    title: (
+      <p>
+        JPMorgan Chase & Co. Code for Good 2022 <strong>Winner</strong>
+      </p>
+    ),
+    desc: (
+      <ul>
+        <li>
+          Developed a platform for beneficiaries to effectively find solutions
+          which are catered specially to their needs
+        </li>
+        <li>
+          Streamlined form filling process and automated form submission to
+          various organisation
+        </li>
+        <li>
+          Provided SG Assist and other non-profit organisations with ease of
+          connection with their beneficiaries
+        </li>
+      </ul>
+    ),
+    image: goldmansachs,
+  },
+  {
+    title: (
+      <p>
+        GoldMan Sachs Engage 2021 <strong>Finalist</strong>
+      </p>
+    ),
+    desc: (
+      <ul>
+        <li>
+          Extracted Unstructured ESG data and performed topic modelling on
+          reports
+        </li>
+        <li>
+          Topic modelling models used include Latent Dirichlet Allocation (LDA){" "}
+        </li>
+        <li>
+          Performed multiclass Classification using Light Gradient Boosting
+          Model
+        </li>
+      </ul>
+    ),
+    image: goldmansachs,
+  },
 ];
 const Achievements = () => {
-    return (
-        <AnimatedPage>
-            <div className="work_div">
-                <div className='expFlex'>
-                    <GrAchievement size={100} color='black' />
-                    <div className="overall_container">
-                    {ACHIEVEMENTS.map((item, index) => (
-                                <div class="container">
-                                    <div class="card" key={index}>
-                                        <div class="card__header">
-                                            <img src={item.image} alt="card__image" class="card__image" width="500"/>
-                                        </div>
-                                        <div class="card_body">
-                                            <h4>{item.title}</h4>
-                                            <p>{item.desc}</p>
-                                        </div>
-                                    </div>
-                                </div> 
-                            ))}
-                    </div>
+  return (
+    <AnimatedPage>
+      <div className="work_div">
+        <div className="expFlex">
+          <GrAchievement size={100} color="black" />
+          <div className="overall_container">
+            {ACHIEVEMENTS.map((item, index) => (
+              <div class="container">
+                <div class="card" key={index}>
+                  <div class="card__header">
+                    <img
+                      src={item.image}
+                      alt="card__image"
+                      class="card__image"
+                      width="500"
+                    />
+                  </div>
+                  <div class="card_body">
+                    <h4>{item.title}</h4>
+                    <p>{item.desc}</p>
+                  </div>
                 </div>
-            </div>
-        </AnimatedPage>      
-        )
-}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </AnimatedPage>
+  );
+};
 
 export default Achievements;
