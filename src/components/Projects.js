6a543422b7b108ecea6fc950c006c91795507d90
gregoryong16 <gregory.ong16@gmail.com>
2022-12-26 16:14:26 +0800

    added new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,69 +1,136 @@
-import React from "react"
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faGithub,} from "@fortawesome/free-brands-svg-icons";
-import {GrGroup} from "react-icons/gr"
-import mdp from "../images/mdp.png"
-import swe from "../images/swe.png"
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { GrGroup } from "react-icons/gr";
+import mdp from "../images/mdp.png";
+import swe from "../images/swe.png";
 import AnimatedPage from "./AnimatedPage";
+import nlp from "../images/nlp.png";
+import db from "../images/db.png";
+import ase from "../images/ase.png";
 
 const PROJECTS = [
-	{
-		title: 'Multidisciplinary Project',
-		desc: (
-			<ul>
-                <li>Configured Raspberry Pi and used it to implement Object Detection model </li>
-                <li>Worked with YOLOv5 and OpenCV</li>
-                <li>Fine tuning of models to achieve desired result (Data augmentation, changing batch size, reducing overfitting, etc)</li>
-                </ul>
-		),
-		image: mdp,
-        url: "https://github.com/gregoryong16/CZ3004-MDP-yolov5"
-	},
-    {
-        title: "Software Engineering Project",
-        desc: (
-            <ul>
-                <li>Developed a phone application that tracks real time utility usage of households </li>
-                <li>Designed the User interface of the application</li>
-                <li>Designed algorithm to query data from database</li>
-            </ul>
-        ),
-        image: swe,
-        url: "https://github.com/gregoryong16/SWE"
-    },
+  {
+    title: "Natural Language Processing",
+    desc: (
+      <ul>
+        <li>Built a sentiment analysis application</li>
+        <li>Crawled and performed polarity detection on tweets from Twitter</li>
+        <li>
+          Tried various models such as BERT, Multilayered Perception and Naive
+          Bayes etc
+        </li>
+      </ul>
+    ),
+    image: nlp,
+    url: "https://github.com/gregoryong16/CZ4045_NLP",
+  },
+  {
+    title: "Advanced Software Engineering",
+    desc: (
+      <ul>
+        <li>
+          Designed a web application to combat the problem of food wastage
+        </li>
+        <li>
+          Web application able to track user's inventory, recommend recipe based
+          on current inventory
+        </li>
+        <li>
+          Web application able to track and display wastage statistics across
+          months
+        </li>
+      </ul>
+    ),
+    image: ase,
+    url: "https://github.com/gregoryong16/CZ3002_ASE",
+  },
+  {
+    title: "Connecting Query with Query Plans",
+    desc: (
+      <ul>
+        <li>Retrieving relevant information from QEP and AQPs</li>
+        <li>Annotate the corresponding SQL query </li>
+        <li>
+          Explain how different components of the query are executed by the
+          underlying query processor and why the operators are chosen among
+          other alternatives
+        </li>
+      </ul>
+    ),
+    image: db,
+    url: "https://github.com/gregoryong16/ADB_Query",
+  },
+  {
+    title: "Multidisciplinary Project",
+    desc: (
+      <ul>
+        <li>
+          Configured Raspberry Pi and used it to implement Object Detection
+          model{" "}
+        </li>
+        <li>Worked with YOLOv5 and OpenCV</li>
+        <li>
+          Fine tuning of models to achieve desired result (Data augmentation,
+          changing batch size, reducing overfitting, etc)
+        </li>
+      </ul>
+    ),
+    image: mdp,
+    url: "https://github.com/gregoryong16/CZ3004-MDP-yolov5",
+  },
+  {
+    title: "Software Engineering Project",
+    desc: (
+      <ul>
+        <li>
+          Developed a phone application that tracks real time utility usage of
+          households{" "}
+        </li>
+        <li>Designed the User interface of the application</li>
+        <li>Designed algorithm to query data from database</li>
+      </ul>
+    ),
+    image: swe,
+    url: "https://github.com/gregoryong16/SWE",
+  },
 ];
 
 const Projects = () => {
-    return (
-        <AnimatedPage>
-            <div className="projects_div">
-                <div className='expFlex'>
-                    <GrGroup size={100} color='black' />
-                    <div className="overall_container">
-                    {PROJECTS.map((item, index) => (
-                                <div class="container">
-                                    <div class="card" key={index}>
-                                        <div class="card__header">
-                                            <img src={item.image} alt="card__image" class="card__image" width="500"/>
-                                        </div>
-                                        <div class="card_body">
-                                            <h4>{item.title}</h4>
-                                            <p>{item.desc}</p>
-                                        </div>
-                                        <div class="projects_git_icon">
-                                            <a href= {item.url}
-                                                className="projects_git_icon">
-                                                <FontAwesomeIcon icon={faGithub} size="2x" />
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div> 
-                            ))}
-                    </div>
+  return (
+    <AnimatedPage>
+      <div className="projects_div">
+        <div className="expFlex">
+          <GrGroup size={100} color="black" />
+          <div className="overall_container">
+            {PROJECTS.map((item, index) => (
+              <div class="container">
+                <div class="card" key={index}>
+                  <div class="card__header">
+                    <img
+                      src={item.image}
+                      alt="card__image"
+                      class="card__image"
+                      width="500"
+                    />
+                  </div>
+                  <div class="card_body">
+                    <h4>{item.title}</h4>
+                    <p>{item.desc}</p>
+                  </div>
+                  <div class="projects_git_icon">
+                    <a href={item.url} class="projects_git_icon_actual">
+                      <FontAwesomeIcon icon={faGithub} size="3x" />
+                    </a>
+                  </div>
                 </div>
-            </div>
-        </AnimatedPage>
-    )
-}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </AnimatedPage>
+  );
+};
 
 export default Projects;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,69 +1,92 @@
-import React from "react"
-import {MdOutlineWork} from 'react-icons/md'
-import {Button,ButtonGroup } from "@chakra-ui/react"
-import amaris from "../images/amaris.png"
-import {AiOutlineArrowRight } from 'react-icons/ai'
+import React from "react";
+import { MdOutlineWork } from "react-icons/md";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import amaris from "../images/amaris.png";
+import { AiOutlineArrowRight } from "react-icons/ai";
 import AnimatedPage from "./AnimatedPage";
-import testimonial from "../files/Amaris Testimonial.pdf"
-import { AiOutlineDownload } from "react-icons/ai"
+import testimonial from "../files/Amaris Testimonial.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
 
 const WORKEXP = [
-	{
-		company: 'Amaris.Ai',
-        position: "AI Intern",
-		date: 'May 2022- Aug 2022',
-		desc: (
-			<p>
-				Automate a complete computer vision model(YOLOv5) for stamp detection project with an actual client for the company<br />
-                <br />
-                Worked with Python, Docker, FastAPI, React and OpenCV
-			</p>
-		),
-		image: amaris,
-        website: 'https://www.amaris.ai/',
-	},
+  {
+    company: "Amaris.Ai",
+    position: "AI Intern",
+    date: "May 2022- Aug 2022",
+    desc: (
+      <ul>
+        <li>
+          Automate a complete computer vision model(YOLOv5) for stamp detection
+          project with an actual client for the company
+        </li>
+        <li>Worked with Python, Docker, FastAPI, React and OpenCV</li>
+      </ul>
+    ),
+    image: amaris,
+    website: "https://www.amaris.ai/",
+  },
 ];
 
 const Work = () => {
-    return (
-		<AnimatedPage>
-			<div className="work_div">
-				<div className='expFlex'>
-                    <MdOutlineWork size={100} color='black' />
-                    <div className="overall_container">
-                        {WORKEXP.map((item, index) => (
-                            <div class="container">
-                                <div class="card" key={index}>
-                                    <div class="card_header">
-                                        <img src={item.image} alt="card__image" class="card__image" width="500"/>
-                                    </div>
-                                    <div class="card_body">
-                                        <span class="tag tag-blue">{item.date}</span>
-                                        <h4>{item.position}</h4>
-                                        {item.desc}
-                                    </div>
-                                    <div class="card_button">
-                                        <div className="card_button_left">
-                                            <ButtonGroup variant='outline' spacing='6'>
-                                                <Button rightIcon = {<AiOutlineDownload/>} colorScheme='BlackAlpha' variant='outline' height='35px'>
-                                                    <a href={testimonial} download="Amaris Testimonial.pdf"> Testimonial </a>
-                                                </Button>
-                                            </ButtonGroup>
-                                        </div>
-                                        <ButtonGroup variant='outline' spacing='6'>
-                                            <Button rightIcon={<AiOutlineArrowRight/>} colorScheme='BlackAlpha' variant='outline' height='35px'>
-                                                <a href={item.website}> {item.company}</a>
-                                            </Button>
-                                        </ButtonGroup>
-                                    </div>
-                                </div>
-                            </div> 
-                        ))}
+  return (
+    <AnimatedPage>
+      <div className="work_div">
+        <div className="expFlex">
+          <MdOutlineWork size={100} color="black" />
+          <div className="overall_container">
+            {WORKEXP.map((item, index) => (
+              <div class="container">
+                <div class="card" key={index}>
+                  <div class="card_header">
+                    <img
+                      src={item.image}
+                      alt="card__image"
+                      class="card__image"
+                      width="500"
+                    />
+                  </div>
+                  <div class="card_body">
+                    <span class="tag tag-blue">{item.date}</span>
+                    <h4>{item.position}</h4>
+                    <p>{item.desc}</p>
+                  </div>
+                  <div class="card_button">
+                    <div className="card_button_left">
+                      <ButtonGroup variant="outline" spacing="6">
+                        <Button
+                          rightIcon={<AiOutlineDownload />}
+                          colorScheme="BlackAlpha"
+                          variant="outline"
+                          height="35px"
+                        >
+                          <a
+                            href={testimonial}
+                            download="Amaris Testimonial.pdf"
+                          >
+                            {" "}
+                            Testimonial{" "}
+                          </a>
+                        </Button>
+                      </ButtonGroup>
                     </div>
-				</div>
-			</div> 
-        </AnimatedPage>
-	);
-}
+                    <ButtonGroup variant="outline" spacing="6">
+                      <Button
+                        rightIcon={<AiOutlineArrowRight />}
+                        colorScheme="BlackAlpha"
+                        variant="outline"
+                        height="35px"
+                      >
+                        <a href={item.website}> {item.company}</a>
+                      </Button>
+                    </ButtonGroup>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </AnimatedPage>
+  );
+};
 
 export default Work;
